b0141583ede80e62e2c20736ecaf732c65cf05f6
Ethan Marks <87040432+ethmarks@users.noreply.github.com>
2026-09-13 11:36:51 -0400

    remove mdToHTML

--- md.js
+++ md.js
@@ -1,53 +1,15 @@
-import { parse, render } from "https://esm.sh/@croct/md-lite@0.3.1";
+import { parse } from "https://esm.sh/@croct/md-lite@0.3.1";
 
 /**
  * Main export
  *
- * @todo Switch from mdToHTML to mdToCreateEl
- *
  * @param {string} markdown
  * @returns {HTMLElement}
- *
  */
 export const mdToEl = mdToCreateEl;
 
 /**
- * Renders Markdown to an HTML string, then uses innerHTML to turn it into an
- * element.
- *
- * Definitely not allowed in the ship.
- *
- * @todo Delete
- *
- * @see {@link https://github.com/croct-tech/md-lite-js/blob/16a25261b28a4bd6a5beaebe2f089d207a9565f6/README.md?plain=1#L83-L93|Source}
- *
- * @param {string} markdown
- * @returns {HTMLElement}
- */
-function mdToHTML(markdown) {
-	const root = document.createElement("div");
-
-	const html = render(markdown, {
-		fragment: (node) => node.children.join(""),
-		text: (node) => node.content,
-		bold: (node) => `<b>${node.children}</b>`,
-		italic: (node) => `<i>${node.children}</i>`,
-		strike: (node) => `<s>${node.children}</s>`,
-		code: (node) => `<code>${node.content}</code>`,
-		link: (node) => `<a href="${node.href}">${node.children}</a>`,
-		image: (node) => `<img src="${node.src}" alt="${node.alt}">`,
-		paragraph: (node) => `<p>${node.children.join("")}</p>`,
-	});
-
-	root.innerHTML = html;
-
-	return root;
-}
-
-/**
  * Parses Markdown into an AST, then assembles an element based on the AST.
- *
- * @todo Implement
  *
  * @param {string} markdown
  * @returns {HTMLElement}
