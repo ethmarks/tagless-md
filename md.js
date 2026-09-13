a56a63a65c119fd643c7272d3b31609b0b9a4af8
Ethan Marks <87040432+ethmarks@users.noreply.github.com>
2026-09-13 11:07:52 -0400

    refactor md.js

--- md.js
+++ md.js
@@ -1,16 +1,32 @@
 import { parse, render } from "https://esm.sh/@croct/md-lite@0.3.1";
 
 /**
+ * Main export
+ *
+ * @todo Refactor to not use mdToHTML
+ *
+ * @param {string} markdown
+ * @returns {HTMLElement}
+ *
+ */
+export const mdToEl = mdToHTML;
+
+/**
+ * Renders Markdown to an HTML string, then uses innerHTML to turn it into an
+ * element.
+ *
+ * Definitely not allowed in the ship.
+ *
+ * @todo Delete
+ *
+ * @see {@link https://github.com/croct-tech/md-lite-js/blob/16a25261b28a4bd6a5beaebe2f089d207a9565f6/README.md?plain=1#L83-L93|Source}
  *
  * @param {string} markdown
  * @returns {HTMLElement}
  */
-export function mdToEl(markdown) {
+function mdToHTML(markdown) {
 	const root = document.createElement("div");
 
-	// [TODO] refactor mdToEl to use .createElement rather than innerHTML
-	//
-	// source: https://github.com/croct-tech/md-lite-js/blob/16a25261b28a4bd6a5beaebe2f089d207a9565f6/README.md?plain=1#L83-L93
 	const html = render(markdown, {
 		fragment: (node) => node.children.join(""),
 		text: (node) => node.content,
