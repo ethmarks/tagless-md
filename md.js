351b676c2a4e32fc0b64327e75bb3bfbd92ea894
Ethan Marks <87040432+ethmarks@users.noreply.github.com>
2026-09-13 11:35:44 -0400

    implement other ast node types in processChild()

--- md.js
+++ md.js
@@ -73,16 +73,45 @@ function processChild(node, parent) {
 		case "text":
 			parent.appendChild(document.createTextNode(node.content));
 			break;
+		case "bold":
+			const bold = document.createElement("strong");
+			processChild(node.children, bold);
+			parent.appendChild(bold);
+			break;
+		case "italic":
+			const italic = document.createElement("em");
+			processChild(node.children, italic);
+			parent.appendChild(italic);
+			break;
+		case "strike":
+			const strike = document.createElement("s");
+			processChild(node.children, strike);
+			parent.appendChild(strike);
+			break;
+		case "code":
+			const code = document.createElement("code");
+			code.appendChild(document.createTextNode(node.content));
+			parent.appendChild(code);
+			break;
 		case "link":
 			const link = document.createElement("a");
 			link.href = node.href;
 			processChild(node.children, link);
 			parent.appendChild(link);
 			break;
-		case "code":
-			const code = document.createElement("code");
-			code.appendChild(document.createTextNode(node.content));
-			parent.appendChild(code);
+		case "image":
+			const image = document.createElement("img");
+			image.src = node.src;
+			image.alt = node.alt;
+			parent.appendChild(image);
+			break;
+		case "paragraph":
+			const paragraph = document.createElement("p");
+			node.children.forEach((child) => processChild(child, paragraph));
+			parent.appendChild(paragraph);
+			break;
+		case "fragment":
+			node.children.forEach((child) => processChild(child, parent));
 			break;
 	}
 }
