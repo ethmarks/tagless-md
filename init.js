4b23ae1865d5ec3585e08912564226615ffd4d80
Ethan Marks <87040432+ethmarks@users.noreply.github.com>
2026-09-16 23:03:43 -0400

    add main

--- init.js
+++ init.js
@@ -1,8 +1,9 @@
-import sheet from "./base.css" with { type: "css" };
+import baseSheet from "./base.css" with { type: "css" };
+import mainSheet from "./main.css" with { type: "css" };
 import { mdToEl } from "./md.js";
 
 // load tiny-brutalism.css
-document.adoptedStyleSheets = [sheet];
+document.adoptedStyleSheets = [baseSheet, mainSheet];
 
 // construct the page content
 const container = document.createElement("div");
@@ -27,38 +28,33 @@ container.appendChild(
 		"Tagless Markdown is a tool built for [Tagless](https://tagless.hackclub.com/) YSWS that renders Markdown in the browser using `.createElement()` to manipulate the DOM. The source code only contains minimal HTML boilerplate, and everything is done with JS.",
 	),
 );
+container.appendChild(document.createElement("hr"));
 
-container.appendChild(
-	mdToEl(
-		`
----
+const main = document.createElement("main");
 
-This is a test.
+const inpt = document.createElement("div");
+const inptLabel = document.createElement("label");
+inptLabel.appendChild(document.createTextNode("Input"));
+inptLabel.htmlFor = "md-textarea";
+inpt.appendChild(inptLabel);
+const textarea = document.createElement("textarea");
+textarea.id = "md-textarea";
+textarea.rows = 25;
+inpt.appendChild(textarea);
+main.appendChild(inpt);
 
-**Bolded** text. _Italic_ text. [Link text](#).
+const out = document.createElement("div");
+const outLabel = document.createElement("label");
+outLabel.appendChild(document.createTextNode("Output"));
+outLabel.htmlFor = "md-out";
+out.appendChild(outLabel);
+const outArea = document.createElement("div");
+outArea.id = "md-out";
+outArea.classList.add("box");
+out.appendChild(outArea);
+main.appendChild(out);
 
-> Blockquote
-
-\`\`\`js
-console.log("testing code blocks");
-console.log("this one is multi-line!");
-\`\`\`
-
-- item a
-	- nested item i
-	- nested item ii
-	- nested item iii
-- item b
-
-1. item 1
-2. item 2
-
-![cat](https://cataas.com/cat)
-
----
-		`,
-	),
-);
+container.appendChild(main);
 
 const footer = document.createElement("footer");
 footer.classList.add("text-center");
