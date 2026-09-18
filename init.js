94f7731a457b596eb707ab74666865cb9f85c577
Ethan Marks <87040432+ColourlessSpearmint@users.noreply.github.com>
2026-09-17 20:54:47 -0400

    fix init.js
    I forgot that I couldn't encapsulate the page elements because they're
    referenced in render(). Whoopsie

--- init.js
+++ init.js
@@ -11,62 +11,54 @@ import initialMd from "./initial.md" with { type: "text" };
 // actually add the stylesheets to the page
 document.adoptedStyleSheets = [baseSheet, mainSheet];
 
-/**
- * creates a div that contains all of the page content and structure
- *
- * @returns {HTMLDivElement}
- */
-function getContainer() {
-  const container = document.createElement("div");
-  container.classList.add("container");
+// create all the page elements
+const container = document.createElement("div");
+container.classList.add("container");
 
-  const title = document.createElement("h1");
-  title.appendChild(document.createTextNode("Tagless Markdown"));
-  title.classList.add("text-center");
-  container.appendChild(title);
+const title = document.createElement("h1");
+title.appendChild(document.createTextNode("Tagless Markdown"));
+title.classList.add("text-center");
+container.appendChild(title);
 
-  const tagline = document.createElement("div");
-  tagline.appendChild(
-    document.createTextNode("A Markdown renderer without a single HTML tag"),
-  );
-  tagline.classList.add("box");
-  tagline.classList.add("text-center");
-  tagline.id = "tagline";
-  container.appendChild(tagline);
+const tagline = document.createElement("div");
+tagline.appendChild(
+  document.createTextNode("A Markdown renderer without a single HTML tag"),
+);
+tagline.classList.add("box");
+tagline.classList.add("text-center");
+tagline.id = "tagline";
+container.appendChild(tagline);
 
-  container.appendChild(document.createElement("hr"));
+container.appendChild(document.createElement("hr"));
 
-  const main = document.createElement("main");
+const main = document.createElement("main");
 
-  const inpt = document.createElement("div");
-  const inptLabel = document.createElement("label");
-  inptLabel.appendChild(document.createTextNode("Input"));
-  inpt.appendChild(inptLabel);
-  const textarea = document.createElement("textarea");
-  textarea.rows = 20;
-  textarea.value = initialMd;
-  inpt.appendChild(textarea);
-  main.appendChild(inpt);
+const inpt = document.createElement("div");
+const inptLabel = document.createElement("label");
+inptLabel.appendChild(document.createTextNode("Input"));
+inpt.appendChild(inptLabel);
+const textarea = document.createElement("textarea");
+textarea.rows = 20;
+textarea.value = initialMd;
+inpt.appendChild(textarea);
+main.appendChild(inpt);
 
-  const out = document.createElement("div");
-  const outLabel = document.createElement("label");
-  outLabel.appendChild(document.createTextNode("Output"));
-  out.appendChild(outLabel);
-  const article = document.createElement("article");
-  out.appendChild(article);
-  main.appendChild(out);
+const out = document.createElement("div");
+const outLabel = document.createElement("label");
+outLabel.appendChild(document.createTextNode("Output"));
+out.appendChild(outLabel);
+const article = document.createElement("article");
+out.appendChild(article);
+main.appendChild(out);
 
-  container.appendChild(main);
+container.appendChild(main);
 
-  const footer = document.createElement("footer");
-  footer.classList.add("text-center");
-  footer.appendChild(mdToEl("By [Ethan Marks](https://github.com/ethmarks)"));
-  container.appendChild(footer);
+const footer = document.createElement("footer");
+footer.classList.add("text-center");
+footer.appendChild(mdToEl("By [Ethan Marks](https://github.com/ethmarks)"));
+container.appendChild(footer);
 
-  return container;
-}
-
-document.body.appendChild(getContainer());
+document.body.appendChild(container);
 
 /**
  * Renders the textarea's content and injects it into the article
